f2a6f3023dc53ffc7e803e87d3b931c0e439717c
MaxVRG <max.vargasp@duocuc.cl>
2024-07-03 20:23:56 -0400

    All templates ready
    Todas las paginas html listas y se muestran correctamente

--- brymaxshop/shop/urls.py
+++ brymaxshop/shop/urls.py
@@ -3,4 +3,10 @@ from .import views
 
 urlpatterns = [
     path('', views.index,name="index"),
+    path('nuevos/', views.nuevos, name='nuevos'),
+    path('usados/', views.usados, name='usados'),
+    path('mantencion/', views.mantencion, name='mantencion'),
+    path('consolas/', views.consolas, name='consolas'),
+    path('info/', views.info, name='info'),
+    path('contacto/', views.contacto, name='contacto'),
 ]
--- brymaxshop/shop/views.py
+++ brymaxshop/shop/views.py
@@ -4,3 +4,21 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'shop/index.html')
+
+def nuevos(request):
+    return render(request, 'shop/nuevos.html')
+
+def usados(request):
+    return render(request, 'shop/usados.html')
+
+def mantencion(request):
+    return render(request, 'shop/mantencion.html')
+
+def consolas(request):
+    return render(request, 'shop/consolas.html')
+
+def info(request):
+    return render(request, 'shop/info.html')
+
+def contacto(request):
+    return render(request, 'shop/contacto.html')
